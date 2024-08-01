f738b84ccc79ad07df81cd77d185668f687e9cd4
SatyakiB7076 <satyakit1998@gmail.com>
2024-08-01 17:18:50 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+const http = require('http').Server(app);
+const cors = require('cors');
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+}); 
+
+//New imports
+
+let users = [];
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on('message', (data) => {
+    socketIO.emit('messageResponse', data);
+  });
+  socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+  //Listens when a new user joins the server
+  socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+    socket.disconnect();
+  });
+});
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Welcome to WebChat',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
